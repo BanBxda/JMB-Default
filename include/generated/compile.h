d44265b465cc7f58e6ee9eef4ffd6dbfd1c760e5
BanBxda <banbxda@gmail.com>
2013-07-02 14:50:25 +0200

    Clean up

--- include/generated/compile.h
+++
@@ -1,7 +0,0 @@
-/* This file is auto generated, version 3 */
-/* SMP PREEMPT */
-#define UTS_MACHINE "arm"
-#define UTS_VERSION "#3 SMP PREEMPT Wed Jun 26 16:35:53 CEST 2013"
-#define LINUX_COMPILE_BY "phil"
-#define LINUX_COMPILE_HOST "BanB-desktop"
-#define LINUX_COMPILER "gcc version 4.4.3 (GCC) "
--- include/generated/utsrelease.h
+++
@@ -1,1 +0,0 @@
-#define UTS_RELEASE "3.0.83-JMB-Default-r7.2"
